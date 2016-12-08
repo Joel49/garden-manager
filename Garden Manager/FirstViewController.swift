27197f2dba11166beda3502f3de8ce4fbbd0341e
Joel <Joel@Joels-MacBook-3.local>
2016-12-08 11:25:04 +0000

    Initial Commit

---
+++ Garden Manager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Garden Manager
+//
+//  Created by Joel Powell on 08/12/2016.
+//  Copyright © 2016 Joel Powell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
